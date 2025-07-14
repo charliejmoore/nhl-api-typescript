2cab5e0f2f3dadbc751e8e5e6d941876b43f6014
Charlie Moore <moorejchar@gmail.com>
2025-07-13 18:18:40 -0600

    🏷️ Update language codes

--- src/types/shared/localizedString.ts
+++ src/types/shared/localizedString.ts
@@ -1,7 +1,15 @@
-/** Generic map for language-localized strings (e.g., default, fr). */
+/** Generic map for language-localized strings (e.g., default, fr). Note: other language codes may be supported as well https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes.*/
 export interface LocalizedString {
   /** English/default value. */
   default: string;
   /** French value, if present. */
   fr?: string;
+  /** Czech value, if present. */
+  cs?: string;
+  /** Finnish value, if present. */
+  fi?: string;
+  /** Slovak value, if present. */
+  sk?: string;
+  /** Swedish value, if present. */
+  sv?: string;
 }
