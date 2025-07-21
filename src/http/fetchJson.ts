0db9aaf93bde50452e2e9ebd2da92a7be930887a
Charlie Moore <moorejchar@gmail.com>
2025-07-21 08:18:08 -0600

    📝 Add documentation

--- src/http/fetchJson.ts
+++ src/http/fetchJson.ts
@@ -1,3 +1,13 @@
+/**
+ * Basic HTTP fetching utility.
+ *
+ * @export
+ * @template T
+ * @param {string} url - URL to make the query to.
+ * @param {RequestInit} [options] - Any query options.
+ * @throws {Error} - Throws error if response is not "ok" i.e., in the 200s. @see https://developer.mozilla.org/en-US/docs/Web/API/Response/ok.
+ * @return {Promise<T>} Returns the response in JSON format.
+ */
 export async function fetchJson<T>(
   url: string,
   options?: RequestInit
