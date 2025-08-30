877a99c0f877bf77e7500dd7636635b6302009e8
Charlie Moore <moorejchar@gmail.com>
2025-08-30 12:32:29 -0600

    📝 Add docs

--- src/types/goalieStatLeaders.ts
+++ src/types/goalieStatLeaders.ts
@@ -2,9 +2,13 @@ import { CurrentStatLeader } from './shared';
 
 /** Shape of data returned from `https://api-web.nhle.com/v1/goalie-stats-leaders/current`. */
 export interface CurrentGoalieStatLeadersResponse {
+  /** List of current leaders in number of wins. */
   wins: CurrentStatLeader[];
+  /** List of current leaders in number of shutouts. */
   shutouts: CurrentStatLeader[];
+  /** List of current leaders in save percentage. */
   savePctg: CurrentStatLeader[];
+  /** List of current leaders in number of goals against average. */
   goalsAgainstAverage: CurrentStatLeader[];
 }
 
