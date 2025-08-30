bf84ab5275376aaaf0e7276bf6beced8a134f8b8
Charlie Moore <moorejchar@gmail.com>
2025-08-30 10:06:15 -0600

    📝 Add documentation

--- src/utilities/isTeamActive.ts
+++ src/utilities/isTeamActive.ts
@@ -1,6 +1,12 @@
 import { currentTeams } from '../constants';
 import { TeamTriCode } from '../types';
 
+/**
+ * Utility function that returns whether or not the given team is currently active in the NHL.
+ *
+ * @param {TeamTriCode} teamAbbreviation - The tricode team abbreviation (e.g., "BUF", "FLA", etc.)
+ * @return {boolean}
+ */
 export const isTeamActive = (teamAbbreviation: TeamTriCode): boolean => {
   return currentTeams.includes(teamAbbreviation);
 };
